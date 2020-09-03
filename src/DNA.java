9338df2d0e1f6e8dbefc135c0c1c57db1b5c9cc8
jurecki0 <sacewiczmathieu@gmail.com>
2020-09-03 13:53:47 +0200

    Added README with explanation instead of explaining it in the code.

--- src/DNA.java
+++ src/DNA.java
@@ -33,13 +33,6 @@ public class DNA {
         //   / \   \   / \   \
         //  ~   `-~ `-`   `-~ `-
 
-    /* For a DNA strand to be a protein, it has to satisfy these 3 conditions:
-    * 1. The beginning of the strand has to be 'ATG'
-    * 2. The end of the strand has to be 'TGA'
-    * 3. The number of nucleotides in between the beginning of the strand and the end of the strand
-    *    has to be a multiple of 3.
-    */
-
         dna = dna1;
         isProtein(dna);
         //space
